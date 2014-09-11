05ccce049a779a0ce945b13b346bc583a0598562
xv1t <xv1t@yandex.ru>
2014-09-11 22:46:26 +0400

    add test ods data array

---
+++ Examples/Continents-array.php
@@ -0,0 +1,179 @@
+array(
+	'Report' => array(
+		'user' => 'www-data',
+		'name' => 'Continents and countries',
+		'number' => (int) 123,
+		'float' => (float) 345.67,
+		'currency' => (float) 56.7
+	),
+	'Continents' => array(
+		(int) 0 => array(
+			'Continent' => array(
+				'name' => 'Asia',
+				'area' => (int) 1234
+			),
+			'Countries' => array(
+				(int) 0 => array(
+					'Country' => array(
+						'name' => 'China',
+						'area' => (float) 123.34
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Pekin',
+								'river' => 'yes'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => 'Shanghai',
+								'river' => 'no'
+							)
+						)
+					)
+				),
+				(int) 1 => array(
+					'Country' => array(
+						'name' => 'India',
+						'area' => (float) 13.34
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Deli',
+								'river' => 'yes'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => 'Jambur',
+								'river' => 'no'
+							)
+						)
+					)
+				)
+			)
+		),
+		(int) 1 => array(
+			'Continent' => array(
+				'name' => 'Europe',
+				'area' => (int) 3452
+			),
+			'Countries' => array(
+				(int) 0 => array(
+					'Country' => array(
+						'name' => 'Russia',
+						'area' => (float) 13.34
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Samara',
+								'river' => 'Volga'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => 'Moscow',
+								'river' => 'yes'
+							)
+						),
+						(int) 2 => array(
+							'City' => array(
+								'name' => 'Penza',
+								'river' => 'yes'
+							)
+						)
+					)
+				)
+			)
+		),
+		(int) 2 => array(
+			'Continent' => array(
+				'name' => 'Africa',
+				'area' => (float) 2311.23
+			),
+			'Countries' => array(
+				(int) 0 => array(
+					'Country' => array(
+						'name' => 'Nigeria',
+						'area' => (float) 34.43
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Main grad',
+								'river' => 'Niger'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => 'Main grad',
+								'river' => 'Niger'
+							)
+						),
+						(int) 2 => array(
+							'City' => array(
+								'name' => 'Main grad',
+								'river' => 'Niger'
+							)
+						)
+					)
+				),
+				(int) 1 => array(
+					'Country' => array(
+						'name' => 'UAR',
+						'area' => (float) 56.43
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Yohanesburg',
+								'river' => 'ocean'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => '9 district',
+								'river' => 'no'
+							)
+						)
+					)
+				),
+				(int) 2 => array(
+					'Country' => array(
+						'name' => 'Egypt',
+						'area' => (float) 87.5
+					),
+					'Cities' => array(
+						(int) 0 => array(
+							'City' => array(
+								'name' => 'Kair',
+								'river' => 'Nill'
+							)
+						),
+						(int) 1 => array(
+							'City' => array(
+								'name' => 'Sharm and sheih',
+								'river' => 'sea'
+							)
+						),
+						(int) 2 => array(
+							'City' => array(
+								'name' => 'Kair',
+								'river' => 'Nill'
+							)
+						),
+						(int) 3 => array(
+							'City' => array(
+								'name' => 'Sharm and sheih',
+								'river' => 'sea'
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+)
